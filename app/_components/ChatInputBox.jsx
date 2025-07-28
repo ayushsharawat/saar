21141a566b083703bd973eda978147cb71139755
ayushsharawat <sharawataayush29@gmail.com>
2025-07-28 12:01:17 +0530

    Logo & input box started

--- app/_components/ChatInputBox.jsx
+++ app/_components/ChatInputBox.jsx
@@ -4,10 +4,11 @@ import React from 'react'
 
 export default function ChatInputBox() {
   return (
-    <div className=" h-screen flex items-center justify-center w-full">
+    <div className=" h-screen flex flex-col items-center justify-center w-full">
       <Image src="/logoname.png" alt="logo" height={300} width={300} />
-      <div>
-        
+      <div className="p-2 w-full max-w-2xl border rounded-2xl">
+        <input type="text" placeholder="Ask Anything" 
+        className='w-full p-4 outline-none' />
       </div>
     </div>
   )
