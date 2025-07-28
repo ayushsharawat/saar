3910bffd87e568d6af281c229da0db8e4ef15f59
ayushsharawat <sharawataayush29@gmail.com>
2025-07-28 14:23:01 +0530

    Till Home page

---
+++ Services/Shared.jsx
@@ -0,0 +1,33 @@
+export const AIModelsOptions = [
+    {
+        id: 1,
+        name: 'Claude 3.5 Sonnet',
+        desc: 'Smart model by Anthropic',
+        modelApi:''
+    },
+    {
+        id: 2,
+        name: 'GPT-4o',
+        desc: 'Smart model by OpenAI',
+        modelApi:''
+    },
+    {
+        id: 3,
+        name: 'Gemini 2.0 Flash',
+        desc: 'Fast model by Google for reasoning',
+        modelApi:''
+    },
+    {
+        id: 4,
+        name: 'Sonar',
+        desc: 'Fast model by Perplexity',
+        modelApi:''
+    },
+    {
+        id: 5,
+        name: 'Grok',
+        desc: 'Fast model by X',
+        modelApi:''
+    }
+    
+]
--- app/_components/ChatInputBox.jsx
+++ app/_components/ChatInputBox.jsx
@@ -4,6 +4,15 @@ import React from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Atom, AudioLines, Cpu, Globe, Mic, Paperclip, SearchCheck } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { AIModelsOptions } from '@/Services/Shared'
 
 export default function ChatInputBox() {
   return (
@@ -23,17 +32,32 @@ export default function ChatInputBox() {
 
           </Tabs>
           <div className='flex gap-0 items-center'>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger><Button variant={'ghost'}>
+                <Cpu className={'text-gray-500 h-5 w-5'} />
+              </Button></DropdownMenuTrigger>
+              <DropdownMenuContent>
+                {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator /> */}
+                {AIModelsOptions.map((model, index) => (
+                  <DropdownMenuItem key={index} className={'mb-1'}>
+                    <div>
+                      <h2 className='text-smaller'>{model.name}</h2>
+                      <p className='text-xs'>{model.desc}</p>
+                    </div>
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
             <Button variant={'ghost'}>
-            <Cpu className={'text-gray-500 h-5 w-5'} />
+              <Globe className={'text-gray-500 h-5 w-5'} />
             </Button>
             <Button variant={'ghost'}>
-            <Globe className={'text-gray-500 h-5 w-5'} />
+              <Paperclip className={'text-gray-500 h-5 w-5'} />
             </Button>
             <Button variant={'ghost'}>
-            <Paperclip className={'text-gray-500 h-5 w-5'} />
-            </Button>
-            <Button variant={'ghost'}>
-            <Mic className={'text-gray-500 h-5 w-5'} />
+              <Mic className={'text-gray-500 h-5 w-5'} />
             </Button>
             <Button>
               <AudioLines className={'text-white h-5 w-5'} />
